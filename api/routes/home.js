47074f8706cb84c93780ffe1abec76ee2b01a50a
JesseDavda <jessepilot@live.com>
2019-08-03 15:15:28 +0100

    Completes changes made in pr #9

--- api/routes/home.js
+++ api/routes/home.js
@@ -2,7 +2,6 @@
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.status(200).json({
-            version: 1.0,
             message: "Ayyy the first route works!!"
         });
     });
--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-const express = requrie('express');
+const express = require('express');
 
 const app = express();
 
