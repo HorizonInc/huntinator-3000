03e265156e1951e387303c5584ef2355b9f4b3f5
JesseDavda <jessepilot@live.com>
2019-08-03 16:04:20 +0100

    PR requests

--- api/routes/home.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.status(200).json({
-            message: "Ayyy the first route works!!"
-        });
-    });
-}
--- api/server.js
+++ api/server.js
@@ -4,7 +4,6 @@ const app = express();
 app.use(express.static('../web/build'));
 
 //Route includes
-require("./routes/home.js");
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
