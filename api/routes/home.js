6d31e1389184db714d405bf7fff03e16b07eb5ff
JesseDavda <jessepilot@live.com>
2019-08-03 16:05:40 +0100

    Uses the express.static function to server the react app build (#13)
    * Uses the express.static function to server the react app build
    
    * PR requests

--- api/routes/home.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.status(200).json({
-            message: "Ayyy the first route works!!"
-        });
-    });
-}
--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const app = express();
 
+app.use(express.static('../web/build'));
+
 //Route includes
-require("./routes/home.js");
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
