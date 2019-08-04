a4b101af09d687ec4194b4aca8488d96652bdf8c
JesseDavda <jessepilot@live.com>
2019-08-04 15:54:07 +0100

    Builds the generate route helper function

--- api/lib/utils.js
+++ api/lib/utils.js
@@ -7,7 +7,7 @@ function getPlacesList(lat, lng, radius) {
 }
 
 function getRandomRiddle() {
-    const raw_JSON = fs.readFileSync(__dirname + '/riddles.json');
+    const raw_JSON = fs.readFileSync(__dirname + '../assets/riddles.json');
     const riddles = JSON.parse(raw_JSON).riddles;
     const max = 129;
 
@@ -16,7 +16,48 @@ function getRandomRiddle() {
     return riddles[position];
 }
 
+function getRandomPlace() {
+    const raw_places_JSON = fs.readFileSync(__dirname + '../assets/places.json');
+    const places_object = JSON.parse(raw_places_JSON);
+    const max = 70;
+
+    let position = Math.round(Math.random() * max);
+
+    return places_object.places[position];
+}
+
+/*
+This is the function that will return the final Route array.
+
+@param number_of_questions, integer
+@param location, object, { latitude: float, longitude: float }
+*/
+function genRoute(number_of_questions, location) {
+    let route_array = [];
+
+    for(let i = 0; i < number_of_questions; i++) {
+        let point = getRandomPlace();
+        let riddle = getRandomRiddle();
+
+        let pointObject = {
+            location: {
+                lat: point.location.lat,
+                lng: point.location.lng
+            },
+            puzzle: {
+                question: riddle[0],
+                answer: riddle[1]
+            }
+        }
+
+        route_array.push(pointObject);
+    }
+
+    console.log(route_array);
+}
+
 export {
     getRandomRiddle,
-    getPlacesList
+    getPlacesList,
+    genRoute
 };
