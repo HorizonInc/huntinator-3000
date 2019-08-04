c04ad12123b5d15ef1089f17dc468ed7175ed178
JesseDavda <jessepilot@live.com>
2019-08-04 16:37:42 +0100

    Removes the /getRandomRiddle route

--- api/routes/riddle.js
+++ api/routes/riddle.js
@@ -4,18 +4,6 @@ import { genRoute } from '../lib/utils.js';
 
 const router = express.Router();
 
-const raw_JSON = fs.readFileSync(__dirname + '/../assets/riddles.json');
-const riddles = JSON.parse(raw_JSON).riddles;
-const max = 129;
-
-router.get('/randomRiddle', (req, res) => {
-    let position = Math.round(Math.random() * max);
-
-    res.status(200).json({
-        riddle: riddles[position]
-    });
-})
-
 router.get('/testGenRoute', (req, res) => {
     console.log("The route array: ", genRoute(20, {}));
 })
