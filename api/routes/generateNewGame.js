16c9c6708241ea569e8a8ede43c5350722c07084
JesseDavda <jessepilot@live.com>
2019-08-06 22:22:15 +0100

    Adds saving the generated game to the database - on error status is set to 500 and error message returned

--- api/routes/generateNewGame.js
+++ api/routes/generateNewGame.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import { genRoute, genGameId } from '../lib/utils.js'; 
-import { read } from 'fs';
+import { Hunt } from '../db/models.js';
 
 const jsonParser = bodyParser.json();
 
@@ -13,13 +13,30 @@ router.post('/newGame', jsonParser, (req, res) => {
     //if(req.body.location === undefined) { res.status(400).json({error_message: "The location for the hunt was not specified"}).end() } Not needed right now as only doing Brighton to start
 
     const number_of_questions = req.body.number_of_questions;
-    const responseObject = {}
-    responseObject.route = genRoute(number_of_questions, {});
-    responseObject.huntId = genGameId();
-    responseObject.teams = [];
 
-    res.status(200).json({
-        new_game_object: responseObject
+    const responseObject = {
+        route: genRoute(number_of_questions, {}), // Second @param of the genRoute function is left blank for now as API only works for Brighton and Hove
+        huntId: genGameId(),
+        teams: [] // Left as a blank array during generation - will be filled dynamically as players join/leave
+    }
+
+    const newHunt = new Hunt(responseObject);
+
+    Hunts.save().then((result) => {
+
+        res.status(200).json({
+            new_game_object: responseObject
+        }).end();
+
+    }).catch(e => {
+
+        console.log("Error saving the generated hunt: ", e);
+
+        res.status(500).json({
+            status: "Error",
+            error_message: e
+        }).end();
+
     });
 });
 
