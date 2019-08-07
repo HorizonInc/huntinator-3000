1a8ef2d315cd565fad57e251ba6611b45ce94a7e
Samanimkr <samanimkr@outlook.com>
2019-08-07 21:14:37 +0100

    Completes  the design f or  the p age

--- web/src/pages/Home/Home.js
+++ web/src/pages/Home/Home.js
@@ -8,11 +8,23 @@ import styles from './Home.style';
 
 class Home extends Component {
     render() {
+        const maxGameCodeLength = '5';
+
         return (
             <div style={styles.container}>
-                <div style={styles.heading}>
+                <div style={styles.headingContainer}>
                     <h1 style={styles.headingText}>Huntinator</h1>
                     <h1 style={styles.headingText}>3000</h1>
+                </div>
+
+                <div style={styles.gameCodeContainer}>
+                    <span style={styles.enterGameCodeText}>enter game code</span>
+                    <input
+                        type="number"
+                        name="game_code"
+                        style={styles.gameCodeInput}
+                        maxLength={maxGameCodeLength}
+                    />
                 </div>
 
                 <div style={styles.generateBtnContainer}>
--- web/src/pages/Home/Home.style.js
+++ web/src/pages/Home/Home.style.js
@@ -1,24 +1,57 @@
+const GEN_BUTTON_HEIGHT = 60;
+const GEN_BUTTON_PADDING = 20;
+
+const CODE_CONTAINER_MARGIN = GEN_BUTTON_HEIGHT + (GEN_BUTTON_PADDING * 2);
+
 const styles = {
     container: {
         height: '100%',
         position: 'relative',
-    },
-    heading: {
         textAlign: 'center',
-        padding: '55px 0',
+        display: 'flex',
+        flexDirection: 'column',
     },
-    headingText: { fontSize: '44px' },
+    headingContainer: {
+        padding: '45px 0',
+    },
+    headingText: {
+        fontSize: '40px',
+    },
+    gameCodeContainer: {
+        flex: 1,
+        paddingTop: '90px',
+        marginBottom: `${CODE_CONTAINER_MARGIN}px`,
+    },
+    enterGameCodeText: {
+        fontSize: '20px',
+        display: 'block',
+    },
+    gameCodeInput: {
+        maxWidth: '80%',
+        margin: '10px auto',
+        padding: '5px',
+        textAlign: 'center',
+        fontSize: '20px',
+        outline: 'none',
+        border: 'none',
+        borderBottom: '2px solid #f0f0f0',
+    },
     generateBtnContainer: {
-        position: 'absolute',
+        position: 'relative',
         bottom: 0,
         left: 0,
-        padding: '20px',
+        padding: `${GEN_BUTTON_PADDING}px`,
         width: '100%',
     },
     generateBtn: {
         width: '100%',
-        height: '80px',
+        height: `${GEN_BUTTON_HEIGHT}px`,
         fontSize: '20px',
+        backgroundColor: '#000',
+        color: '#fff',
+        outline: 'none',
+        border: 'none',
+        borderRadius: '3px',
     },
 };
 
