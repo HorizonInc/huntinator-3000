9d7567624c917f3f85c9ae3f421b5f9adba4ce98
Samanimkr <Samanimkr@outlook.com>
2019-08-10 21:27:49 +0100

    script error

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+
+# Build web app
+cd web
+yarn run build
+
+# Build server
+cd ../api
+yarn run build
+
+# Go back to root folder
+cd ..
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,8 @@
 
 COMMIT_MSG=$1
 
-# Build web app 
-yarn run build
-
 # Commit changes and push to master
-git add ./build/*
+git add .
 git commit -m "DEPLOY - $COMMIT_MSG" 
 git push origin master
 
